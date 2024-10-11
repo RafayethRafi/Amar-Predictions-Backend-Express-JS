7e10c9e244a8d54f7c483105b0dac5d821e3746d
RafayethRafi <rhrafistudent@gmail.com>
2024-10-12 04:05:27 +0600

    Refactor Express API configuration and file structure

--- api/index.js
+++ api/index.js
@@ -1,32 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const app = require('../index');
 
-const app = express();
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Import routes
-const usersRoutes = require('../routes/users');
-const authRoutes = require('../routes/auth');
-const adminRoutes = require('../routes/admin');
-
-// Use routes
-app.use('/users', usersRoutes);
-app.use('/', authRoutes);
-app.use('/admin', adminRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the API');
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
-// Export the Express API
 module.exports = app;
--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-// run `node index.js` in the terminal
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/users', require('./routes/users'));
+app.use('/', require('./routes/auth'));
+app.use('/admin', require('./routes/admin'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the API');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Export the Express API
+module.exports = app;
